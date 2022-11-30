2dccea9fb1a078be173947b2a9f71cdd7fdcca06
Carlos Roman <carlosroman@Carloss-MacBook-Pro.local>
2022-11-29 20:58:23 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const openBrowser = require("react-dev-utils/openBrowser");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    hot: false,
+    onListening: function (devServer) {
+      if (!devServer) {
+        throw new Error("webpack-dev-server is not defined");
+      }
+      const addr = devServer.server.address();
+      console.log(addr);
+      openBrowser(`http://localhost:${addr.port}`);
+    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
